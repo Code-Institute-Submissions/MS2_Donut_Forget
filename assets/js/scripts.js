e4dcc992d705a3f8b9cf0db43cb18785faa0bd80
monanorremo <mona.norremo@gmail.com>
2020-11-16 10:54:08 +0000

    Restarting game and ending game functionality

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,46 +1,60 @@
 $(document).ready(function () {
 
-    
-
-  
-  let lockGrid = false;
-  
+  let lockGrid = false;  
+  //let firstCard, secondCard;
+  let time = 0, totalSeconds = 0, 
+    minutesLabel = document.getElementById("minutes"),
+    secondsLabel = document.getElementById("seconds");
 
   function startGame(){
 
     $("#start").click(function(){
-        Timer();
-        
-    $(".modalBackground").hide();
-
-
-   });
-
-     }
+        time = setInterval(setTimer, 1000);
+        $(".modalBackground").hide();
+    });
+}
     startGame();
 
 
-  
- //borrowed from https://codepen.io/bycreator/pen/RNQmZK//   
-
-
-  let duration = 0;
-    let time = setInterval(Timer, 1000
-    );
-function Timer(){
-   
-     $("#timer").html(`Timer: ${duration}`);
-    $(".cards").click(function () {
-        if ($(hideMatchedCards).length === 36){
+    function setTimer() {
+        ++totalSeconds;
+        secondsLabel.innerHTML = pad(totalSeconds % 60);
+        minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
+        if (minutesLabel.innerHTML === "03" && secondsLabel.innerHTML === "00") {
             clearInterval(time);
+            restartGame();
         }
+    }
+
+    function pad(val) {
+        let valString = val + "";
+        if (valString.length < 2) {
+            return "0" + valString;
+        } else {
+            return valString;
+        }
+    }
+  
+ //borrowed from https://www.w3schools.com/js/js_timing.asp//   
+
+
+  //let duration = 0;
+    //let time = setInterval(Timer, 1000
+  //  );
+//function Timer(){
+   
+ //    $("#timer").html(`Timer: ${duration}`);
+   /// $(".cards").click(function () {
+      //  if ($(hideMatchedCards).length === 36){
+        //    clearInterval(time);
+     //   }
     
 
     
-});
+//});
 
 
-}
+//}
 
 //modified from https://codepen.io/bycreator/pen/RNQmZK//
 
@@ -58,7 +72,6 @@ function Timer(){
   function selectingCards() {
     if ($(".cards.flip.selected").length === 2) {
       matchingCards();
-      
     }
   }
 
@@ -75,8 +88,7 @@ function Timer(){
   }
 
   function hideMatchedCards(){
-$(".cards.flip.selected").removeClass("selected").animate({opacity: 0}, {duration: 2000});
-
+    $(".cards.flip.selected").removeClass("selected").animate({opacity: 0}, {duration: 2000});
   }
 
   function notMatchedCards() {
@@ -88,13 +100,46 @@ $(".cards.flip.selected").removeClass("selected").animate({opacity: 0}, {duratio
     $(".cards.flip.selected").removeClass("selected");
   }
 
-
   //Fisher-Yates method//
   function shuffle() {
     let cards = document.querySelector(".game_grid");
     for (i = cards.children.length; i > -1; i--) {
-    cards.appendChild(cards.children[(Math.random() * i) | 0]);
+        cards.appendChild(cards.children[(Math.random() * i) | 0]);
     }
   }
   shuffle();
 });
+
+function endOfGame(){
+if ($(hideMatchedCards).length === 36){
+    setTimeout(function (){
+$("#restart").replaceWith(`<div id="restart">
+        <h1>Game Over</h1>
+        <p>Click here to restart!</p>
+        </div>`);
+    },);
+    restartGame();
+}
+
+}
+
+   
+
+
+
+
+
+function restartGame(){
+if (hideMatchedCards ().length === 36);
+ $(".modalBackground").show();
+ $("#restart").click(function(){
+     clearInterval(time);
+        time = setInterval(setTimer, 1000);
+        $(".modalBackground").hide();
+
+
+}
+
+, restartGame());
+
+};
